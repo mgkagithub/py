cebf2eb7f00698b4b5c49b5f4524a14ec3c06ec7
mgkagithub <omegarapid123456789@gmail.com>
2023-06-27 00:20:14 +0530

    ashvanis program reduced 1 line

--- ppps/ashvani.py
+++ ppps/ashvani.py
@@ -3,13 +3,13 @@ s1,s2,l,sam=1,int(input("First step is always 1.\nEnter 2nd step: ")),[1 for _ i
 num=len(l)
 while 1:
     z=set(permutations(l,len(l)))
-    print(z,len(z))
+    print(z,'\n',len(z),'\n')
     sam+=len(z)
     for i in range(s2):
         l.remove(s1)
     l.append(s2)
     if l.count(s1) < s2:
         z=set(permutations(l,len(l)))
-        print(z,len(z))
+        print(z,'\n',len(z),'\n')
         break
-print(sam+len(z))
+print(f"The final sum is {sam+len(z)}")
