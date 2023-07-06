79928fb4eceb7c74546201377e0a831d05da9a4a
mgkagithub <omegarapid123456789@gmail.com>
2023-07-06 21:16:52 +0530

    basic gpt design

--- gpt/gpt.py
+++ gpt/gpt.py
@@ -1,20 +1,17 @@
-import os
+import dotenv
+from dotenv import load_dotenv
 import openai
-import pyttsx3 
-import time 
-openai.organization = "org-4R7gGAwRtsQb3FdDEG3fHVJ6"
-openai.api_key = "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"
-
-conversation = [{"role":"user","content": "you are a helpful assistant"}]
-
-while(True):
-    user_input = input("Prompt: ")
-    conversation.append({"role":"user","content": user_input})
-
-    response = openai.ChatCompletion.create(
-        model = "gpt-3.5-turbo",
-        messages = conversation
-    )
-    conversation.append({"role":"assistant","content": response['choices'][0]['messages']['content']})
-    print("\n"+response['choices'][0]['messages']['content']+"\n")
-
+import os
+dotenv.load_dotenv()
+openai.api_key = os.getenv('OPENAI_KEY')
+prompt = input("Enter a prompt:\n")
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  temperature=1,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+print(response["choices"][0]["text"])
