35df3aca5ca9d6669c13b74e26dd4fba00ff50a4
mgkagithub <omegarapid123456789@gmail.com>
2023-07-08 22:53:30 +0530

    idk

--- ppps/cl.py
+++ ppps/cl.py
@@ -1,12 +1,3 @@
-class Person:
-  def __init__(self, name, age,gender):
-    self.name = name
-    self.age = age+2
-    self.gender = gender
-  def __str__(self):
-    return f"{self.name} \n-> {self.age}\n-> {self.gender}"
-
-p1 = Person("ash", 12,'t327')
-p2 = Person("jois", 19,'m')
-print(p1)
-print(p2)
+class Employee:
+  def __init__(self,name,number):
+    
